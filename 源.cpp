232ec8797c6c42968c1d6773e726b8dd38871073
sixducks <82391600+sixducks@users.noreply.github.com>
2021-10-19 22:11:01 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,34 @@
+//#include<iostream>
+//
+//using namespace std;
+//int main()
+//{
+//	int i, j;
+//	for (i = 1; i < 10; i++)
+//	{
+//		for (j = 1; j < 10; j++)
+//		{
+//			cout << i << "*" << i*j << " ";
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+//#include<iostream>
+//
+//using namespace std;
+//int main()
+//{
+//	char s[100];
+//	cin.getline(s, 100);
+//	int i = 0, num = 0;
+//	while (s[i] != '\0')
+//	{
+//		if (s[i] >= 'a'&& s[i] <= 'z')
+//			num++;
+//		i++;
+//	}
+//	cout << num;
+//	return 0;
+//}
